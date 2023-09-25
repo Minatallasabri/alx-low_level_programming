396b61a4873b210e6b60433baed96c7c4dab90d2
Minatallasabri <minatallasabri3380@gmail.com>
2023-09-25 15:55:44 -0700

    linked lists 2 fixed task *0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t q = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		q++;
 	}
-	return (i);
+	return (q);
 }
