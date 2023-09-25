f8366aec3a4d9e6b0b209fcb580432f70da3e772
Minatallasabri <minatallasabri3380@gmail.com>
2023-09-25 14:26:27 -0700

    linked lists 2 fixed task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints a linked lists
- * @h: pointer to first node 
+ * @h: pointer to first node
  *
  * Return: size of list
  */
@@ -11,10 +11,10 @@ size_t print_listint(const listint_t *h);
 	size_t i = 0;
 
 	while (h)
-	{
-	        printf("%d/n", h->n);
-	        h = h->next;
-		i++;
 	}
+	printf("%d/n", h->n);
+	h = h->next;
+	i++;
+{
 	return (i);
 }
