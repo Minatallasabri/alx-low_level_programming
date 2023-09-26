cc66eb6b823c85492cbc427a18b185dfd026187f
Minatallasabri <minatallasabri3380@gmail.com>
2023-09-26 12:42:10 -0700

    linked lists 2 task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *node, *perv_node;
+	listint_t *node, *prev_node;
 	unsigned int i = 0;
 
 	if (!head || !*head)
